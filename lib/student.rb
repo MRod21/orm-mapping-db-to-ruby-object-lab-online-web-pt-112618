d3a5058f4728c2db3c21039c9b1b68daea1f3147
Mario Rodriguez <mario.rodriguez35@yahoo.com>
2019-02-21 04:11:29 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -68,7 +68,7 @@ class Student
       LIMIT 1
       SQL
       
-      DB[:conn].execute(sql).map do |row|
+      DB[:conn].execute(sql)[0].map do |row|
         self.new_from_db(row)
       end
   end
