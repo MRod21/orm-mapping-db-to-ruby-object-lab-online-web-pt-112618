e75c94fdb1701de718ac2eaa040895e52a0a6a92
Mario Rodriguez <mario.rodriguez35@yahoo.com>
2019-02-21 04:06:12 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -53,7 +53,7 @@ class Student
   end
   
   def self.first_X_students_in_grade_10(number)
-    sql <<-SQL
+    sql = <<-SQL
       SELECT * FROM students WHERE grade = 10 
       LIMIT ?
       SQL
